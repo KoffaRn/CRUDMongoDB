204ea4350eaa3de7993f8a11b1d86b94f3063a0f
Kristoffer Larsson <lilamoln.koffa@gmail.com>
2023-05-03 19:44:14 +0200

    Fixed bug if the type was null in PersonFactory

--- src/main/java/org/koffa/People/PersonFactory.java
+++ src/main/java/org/koffa/People/PersonFactory.java
@@ -3,12 +3,13 @@ import org.bson.Document;
 public class PersonFactory {
     public static Person createPerson(Document document) {
         String type = document.getString("type");
-        if(type.equals("employee")) {
-            return new Employee().fromDocument(document);
-        } else if(type.equals("customer")) {
-            return new Customer().fromDocument(document);
-        } else {
-            return new Person().fromDocument(document);
+        if(type != null) {
+            if(type.equals("employee")) {
+                return new Employee().fromDocument(document);
+            } else if(type.equals("customer")) {
+                return new Customer().fromDocument(document);
+            }
         }
+        return new Person().fromDocument(document);
     }
 }
